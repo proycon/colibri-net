60eb9be41542ea086e2ad69b75914d7905f7e8c4
Maarten van Gompel <proycon@anaproy.nl>
2013-09-24 20:07:35 +0200

    multiprocessing

--- build.py
+++ build.py
@@ -2,7 +2,9 @@
 
 from __future__ import print_function, unicode_literals, division, absolute_import
 import os.path
+import os
 import sys
+from multiprocessing import Pool
 
 #a subselection
 langs = ['en','fr','es','nl','de','it','pt','ru','pl','sv','da','tr','el','fi','id']
@@ -23,16 +25,26 @@ def tokenise(resultbase, lang):
         os.system("ucto -Lgeneric -m -n " + resultbase + '.' + lang + ' > ' + resultbase + '.' + lang + '.tok')
 
 
-for lang in langs:
-    for lang2 in langs:
-        if lang < lang2:
-            archivefile = CORPUSDIR + '/' + lang+"-"+lang2 + ".txt.zip"
-            resultbase = CORPUSDIR + "/OpenSubtitles2012." + lang + "-" + lang2
-            if os.path.exists(archivefile) and not os.path.exists(resultbase + '.' + lang+'.tok') and not os.path.exists(resultbase + '.' + lang2+'.tok'):
-                print("Processing " + lang + "-" + lang2,file=sys.stderr)
-                os.system("unzip " + archivefile)
-                tokenise(resultbase, lang)
+def process(lang, lang2):
+    archivefile = CORPUSDIR + '/' + lang+"-"+lang2 + ".txt.zip"
+    resultbase = CORPUSDIR + "/OpenSubtitles2012." + lang + "-" + lang2
+    print("Processing " + lang + "-" + lang2,file=sys.stderr)
+    os.system("unzip " + archivefile)
+    tokenise(resultbase, lang)
+
+def getpairs():
+    for lang in langs:
+        for lang2 in langs:
+            if lang < lang2:
+                archivefile = CORPUSDIR + '/' + lang+"-"+lang2 + ".txt.zip"
+                resultbase = CORPUSDIR + "/OpenSubtitles2012." + lang + "-" + lang2
+                if os.path.exists(archivefile) and not os.path.exists(resultbase + '.' + lang+'.tok') and not os.path.exists(resultbase + '.' + lang2+'.tok'):
+                    yield lang,lang2
 
 
+threads = int(sys.argv[1])
+if __name__ == '__main__':
+    with Pool(processes=4) as pool:        # start 4 worker processes
+        pool.map(process, getpairs())      # prints "[0, 1, 4,..., 81]"
 
 
