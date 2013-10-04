9828c6588aa2a6fffa6343edabfb30e1e36e3d40
Maarten van Gompel <proycon@anaproy.nl>
2013-10-04 13:37:26 +0200

    fixes and verbosity

--- preparedata.py
+++ preparedata.py
@@ -4,6 +4,7 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
 import os.path
 import os
 import sys
+import datetime
 from multiprocessing import Pool
 
 #a subselection
@@ -19,41 +20,55 @@ os.chdir(CORPUSDIR)
 
 
 def tokenise(resultbase, lang):
+    print("\tTokenising " + resultbase + '.' + lang,file=sys.stderr)
     if lang in specifictokenisers:
-        os.system("ucto -d 1 -L"+lang + " -m -n " + resultbase + '.' + lang + ' > ' + resultbase + '.' + lang + '.tok 2> ' + resultbase + '.' + lang + '.log')
+        r = os.system("ucto -d 1 -L"+lang + " -m -n " + resultbase + '.' + lang + ' > ' + resultbase + '.' + lang + '.tok 2> ' + resultbase + '.' + lang + '.log')
     else:
-        os.system("ucto -d 1 -Lgeneric -m -n " + resultbase + '.' + lang + ' > ' + resultbase + '.' + lang + '.tok 2> ' + resultbase + '.' + lang + '.log' )
+        r = os.system("ucto -d 1 -Lgeneric -m -n " + resultbase + '.' + lang + ' > ' + resultbase + '.' + lang + '.tok 2> ' + resultbase + '.' + lang + '.log' )
+    if r:
+        print("\tERROR: Tokeniser " + resultbase + '.' + lang + " returned with error!! Inspect log!",file=sys.stderr)
+
 
 
 def process(data):
-    lang,lang2 = data
+    num, lang,lang2 = data
     archivefile = CORPUSDIR + '/' + lang+"-"+lang2 + ".txt.zip"
     resultbase = CORPUSDIR + "/OpenSubtitles2012." + lang + "-" + lang2
     expresultbase = EXPDIR + "/OpenSubtitles2012." + lang + "-" + lang2
-    print("Processing " + lang + "-" + lang2,file=sys.stderr)
+    print("Processing pair #" + str(num) + " -- " + lang + "-" + lang2 + " -- " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),file=sys.stderr)
     os.system("unzip " + archivefile)
     os.system("cp " + resultbase + "* " + EXPDIR)
-    tokenise(expresultbase, lang)
-    tokenise(expresultbase, lang2)
-    os.system("rm " + resultbase)
-    os.system("gzip " + resultbase + "." + lang + ".log")
-    os.system("gzip " + resultbase + "." + lang + ".tok")
-    os.system("gzip " + resultbase + "." + lang2 + ".log")
-    os.system("gzip " + resultbase + "." + lang2 + ".tok")
+    if not os.path.exists(expresultbase + '.' + lang + '.tok.gz'):
+        tokenise(expresultbase, lang)
+    if not os.path.exists(expresultbase + '.' + lang2 + '.tok.gz'):
+        tokenise(expresultbase, lang2)
+    #removing untokenised sources
+    os.system("rm " + expresultbase + "." + lang)
+    os.system("rm " + expresultbase + "." + lang2)
+    #gzipping the rest
+    os.system("gzip " + expresultbase + "." + lang + ".log")
+    os.system("gzip " + expresultbase + "." + lang + ".tok")
+    os.system("gzip " + expresultbase + "." + lang2 + ".log")
+    os.system("gzip " + expresultbase + "." + lang2 + ".tok")
+    print("Done " + lang + "-" + lang2 + " -- " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),file=sys.stderr)
 
 def getpairs():
+    num = 0
     for lang in langs:
         for lang2 in langs:
             if lang < lang2:
                 archivefile = CORPUSDIR + '/' + lang+"-"+lang2 + ".txt.zip"
                 resultbase = CORPUSDIR + "/OpenSubtitles2012." + lang + "-" + lang2
-                if os.path.exists(archivefile) and not os.path.exists(resultbase + '.' + lang+'.tok') and not os.path.exists(resultbase + '.' + lang2+'.tok'):
-                    yield lang,lang2
+                if os.path.exists(archivefile) and (not os.path.exists(resultbase + '.' + lang+'.tok.gz') or not os.path.exists(resultbase + '.' + lang2+'.tok.gz')):
+                    num += 1
+                    yield num, lang,lang2
 
 
 threads = int(sys.argv[1])
 if __name__ == '__main__':
-    pool = Pool(processes=threads)        # start 4 worker processes
-    pool.map(process, list(getpairs()))      # prints "[0, 1, 4,..., 81]"
+    pool = Pool(processes=threads)
+    pairs = list(getpairs())
+    print("Found ", len(pairs), " language pairs",file=sys.stderr)
+    pool.map(process, pairs)      # prints "[0, 1, 4,..., 81]"
 
 
